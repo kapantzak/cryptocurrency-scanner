67b3f0886213760dbcc4663361eeacbd31c1a1f2
John Kapantzakis <kapantzak@gmail.com>
2022-10-31 00:47:26 +0200

    Remove unused code

--- components/CoinCommunity/index.js
+++ components/CoinCommunity/index.js
@@ -60,7 +60,6 @@ const CoinCommunity = ({ communityData }) => {
     reddit_average_posts_48h,
     reddit_average_comments_48h,
     reddit_subscribers,
-    reddit_accounts_active_48h,
     telegram_channel_user_count,
   } = communityData;
 
